66848ea2e4af20bed11ee5a705d2f6055ed4bb62
duxianghua <xianghuadu@gmail.com>
2017-06-27 16:39:20 +0800

    test0.1

--- saltfish/service/nodejs.py
+++ saltfish/service/nodejs.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import logging
 import re
 import os
 import sys
+import time
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(BASE_DIR)
@@ -14,47 +16,88 @@ log.setLevel('DEBUG')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-RE_STR = "(?P<APP>\w+)-(?P<GameType>\w+)-\w+-(?P<TableID>\d+).*"
 
 
-def nodejs_service(*args, **kwargs):
+def format_service_name(service):
+    RE_STR = "(?P<AppName>\w+)-(?P<GameType>\w+)-TABLE-(?P<TableID>\d+).*"
+    p = re.match(RE_STR, service).groupdict()
+    s = service.split('.')[0]
+    return s,p
+
+class NodejsService(BaseService):
+    def check(self, parameter):
+        status, rev = self.run('show')
+        l = rev.split('\n')
+        d = {}
+        for i in l:
+            stl = i.split('=', maxsplit=1)
+            d[stl[0]] = stl[1]
+        return d[parameter]
+
+    def stop(self):
+        self.service.run('stop')
+        if self.check('ActiveState') != 'active':
+            self.remove_service()
+            if self.check('LoadState') != 'loaded':
+                log.debug('DELETE SERVICE FILE: %s Done' %self.service)
+                sys.exit(0)
+            else:
+                log.error('DELETE SERVICE FILE: %s Failure' %self.service)
+                sys.exit(101)
+        else:
+            log.error('STOPING SERVICE: %s Failuer')
+            sys.exit(102)
+
+    def start(self):
+        if self.check('LoadState') != 'loaded':
+            if self.create_service(TEMPLATE_DIR, 'p2p-template.service'):
+                log.debug('Create service: %s Done' %self.service)
+            else:
+                log.error('Create service: %s Failure' %self.service)
+                sys.exit(103)
+        self.run('start')
+        if self.check('ActiveState') == 'active':
+            log.debug('Start service: %s Done' % self.service)
+            sys.exit(0)
+        else:
+            log.error('Start service: %s Failure' % self.service)
+            sys.exit(104)
+
+    def kill(self, signal):
+        action = "kill --signal=%s" %signal
+        self.run(action)
+        log.debug('EXEC Command: %s %s' %(action, self.service))
+        time.sleep(1)
+        self.stop()
+
+    def other(self, action):
+        status, rev = self.run(action)
+        log.info(rev)
+        sys.exit(status)
+
+def p2p_service(*args, **kwargs):
         action = kwargs['action']
         signal = kwargs['signal']
         service = kwargs['service']
-        service_kwargs = re.match(RE_STR, service).groupdict()
-        service = BaseService(service, **service_kwargs)
-        if action == 'stop':
-            service.run('stop')
-            service.remove_service()
-        elif action == 'kill' and signal:
-            action_args = "kill --signal=%s" %signal
-            service.run(action_args)
-            service.remove_service()
-        elif action == 'start':
-            if service.service_exists() == False:
-                service.create_service(TEMPLATE_DIR, 'p2p-template.service')
-                log.debug("create service done")
-            status,rev = service.run('start')
-            log.info(rev)
-        elif action in ['restart', 'reload', 'status']:
-            if service.service_exists():
-                status, rev = service.run(action)
-                log.info(rev)
-
+        SName, Sparameter = format_service_name(service)
+        service = NodejsService(SName, **Sparameter)
+        if action == 'start':
+            service.start()
+        elif action == 'stop':
+            service.stop()
+        elif action == 'kill':
+            service.kill(signal=signal)
+        else:
+            service.other(action=action)
 
 def __man__():
     import argparse
     parser = argparse.ArgumentParser(description='Test Process.')
-    parser.add_argument('action', choices=['start',
-                                           'stop',
-                                           'restart',
-                                           'reload',
-                                           'kill',
-                                           'status'])
+    parser.add_argument('action', choices=['start', 'stop', 'restart', 'reload', 'kill', 'status'])
     parser.add_argument('service')
     parser.add_argument('--signal', metavar='signal')
     args = parser.parse_args()
-    nodejs_service(**args.__dict__)
+    p2p_service(**args.__dict__)
 
 if __name__ == '__main__':
     from saltfish.log import setup_console_logger
