318772a3b1d123804a704f139d781755182f8789
duxianghua <xianghuadu@gmail.com>
2017-07-10 13:46:20 +0800

    v0.1.2

--- saltfish/script/nodejs.py
+++ saltfish/script/nodejs.py
@@ -5,18 +5,14 @@ from saltfish.service.NodejsService import GameServiceCLI, GameOptionPares
 
 class Game(GameOptionPares):
     def run(self):
-        self.parse_args(['create','bigtwo-p2p-table-12'])
+        self.parse_args()
         print self.config
         GameServiceCLI(**self.config)
 
 def game_service():
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    print BASE_DIR
     c = GameOptionPares()
     c.parse_args()
     GameServiceCLI(**c.config)
 
 if __name__ == '__main__':
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    print BASE_DIR
     game_service()
